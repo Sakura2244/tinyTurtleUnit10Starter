84639363dfb0b7b40a8a61053dbe018a951d4efc
Jennifer <jenniferzheng23@gmail.com>
2017-03-06 22:10:20 +0000

    used functions

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function square(x){
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
+right(45);
 }
-
+function triangle(){
+forward(50);
+right(90);
+forward(50);
+right(135);
+forward(70);
+}
+function octagon(y){
+forward(y);
+right(45);
+forward(y);
+right(45);
+forward(y);
+right(45);
+forward(y);
+right(45);
+forward(y);
+right(45);
+forward(y);
+right(45);
+forward(y);
+right(45);
+forward(y);
+left(135);
+forward(70);
+}
 // Type your function call below
-
-
+square(70);
+triangle();
+octagon(50);
 stamp();
